afb1af8f40c22ce7f6c7fd274dd0bbf70ce8df05
rabinrai44 <rabinrai44@gmail.com>
2018-08-01 22:00:00 -0400

    Added lab 16

---
+++ CountriesList/CountriesTextFile.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+
+namespace CountriesList
+{
+    public static class CountriesTextFile
+    {
+        //file path
+        private static string path = "/Users/rabin/Desktop/csharpdotnetbootcamp/Lab16-CountriesList/Countries.txt";
+
+        // Read country from file
+        public static void ReadCountries()
+        {
+            try
+            {
+                using (StreamReader reader = new StreamReader(path))
+                {
+                    string line;
+                    // Read and display lines from the file
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        Console.WriteLine(line);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine("Add country to make file.");
+            }
+
+        }
+
+
+        //Write country to file
+        public static void WriteCountries()
+        {
+            Console.WriteLine("enter country");
+            string countryInput = Console.ReadLine();
+
+            using (StreamWriter createFile = File.AppendText(path))
+            {
+                Console.WriteLine("Country Added!");
+                createFile.WriteLine(countryInput);
+            }
+        }
+    }
+}
---
+++ CountriesList/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace CountriesList
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Countries Maintenance Application!\n-------------------------------------------------------");
+
+            //Start running
+            do
+            {
+                try
+                {
+                    Console.WriteLine("\n1 - See the list of contries");
+                    Console.WriteLine("\n2 - Add a country");
+                    Console.WriteLine("\n3 - Exit");
+
+                    Console.Write("Enter menu number: ");
+                    int userInput = int.Parse(Console.ReadLine());
+
+                    switch (userInput)
+                    {
+                        case 1:
+                            CountriesTextFile.ReadCountries();
+                            break;
+                        case 2:
+                            CountriesTextFile.WriteCountries();
+                            break;
+                        case 3:
+                            Console.WriteLine("Goodbye!");
+                            break;
+                        default:
+                            Console.WriteLine("Not valid number you've entered! Please try again.");
+                            break;
+                    }
+                }
+                catch (FormatException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            } while (ShouldContinue());
+        }
+
+        public static bool ShouldContinue()
+        {
+            do
+            {
+                Console.WriteLine("Continue (y/n)?");
+                var input = Char.ToLower(Console.ReadKey().KeyChar);
+                if (input != 'y' && input != 'n')
+                    continue;
+                else
+                    return input == 'y';
+            } while (true);
+        }
+    }
+}
